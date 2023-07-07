2bd103bb1f9b7f6b5be4eca44adcb890966b5ed9
Nhat Huynh <nhatthuynhh621@gmail.com>
2023-07-07 10:44:28 +0700

    update screen sharing

--- dyode-sender/dyode-sender.py
+++ dyode-sender/dyode-sender.py
@@ -6,6 +6,7 @@ from subprocess import Popen, PIPE
 import yaml
 
 import filetransfer
+import screenlistener
 import screensharing
 import syslogforwarding
 
@@ -21,6 +22,9 @@ def launch_agents(module, properties):
     if properties['type'] == 'filetransfer':
         log.debug(f'Instanciating a file transfer module :: {module}')
         filetransfer.watch_folder(properties)
+    elif properties['type'] == 'screen_listener':
+        log.debug(f'Screen listener agent : ${module}')
+        screenlistener.screen_listener(module, properties)
     elif properties['type'] == 'screen':
         log.debug(f'Screen sharing agent : ${module}')
         screensharing.watch_folder(module, properties)
@@ -60,11 +64,10 @@ if __name__ == '__main__':
     for module, properties in modules.items():
         properties['ip_out'] = config['dyode_receiver']['ip']
         properties['interface_in'] = config['dyode_sender']['interface']
-        if properties['in'] and not os.path.exists(properties['in']):
+        if properties.get('in') and not os.path.exists(properties['in']):
             os.makedirs(properties['in'])
-        if properties['temp'] and not os.path.exists(properties['temp']):
+        if properties.get('temp') and not os.path.exists(properties['temp']):
             os.makedirs(properties['temp'])
-        log.debug(f'Parsing {module}')
         log.debug(f'Trying to launch a new process for module {module}')
         p = multiprocessing.Process(
             name=str(module), target=launch_agents, args=(module, properties))
