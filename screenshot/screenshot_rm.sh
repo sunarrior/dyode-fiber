21683d1f3dd21bee04bfd92a6d513f1581d9b8a0
Nhat Huynh <nhatthuynhh621@gmail.com>
2023-07-07 11:37:23 +0700

    update screen sharing

--- screenshot/screenshot_rm.sh
+++ screenshot/screenshot_rm.sh
@@ -14,8 +14,8 @@ do
   # echo "$current_unix_second"
   if [ $screenshot_rm_timeout -eq rm_screenshot_after ]
   then
-    remove_from=((current_unix_second - rm_screenshot_after + 1)) ;
-    remote_end=((remove_from + number_of_image_rm - 1)) ;
+    remove_from=$((current_unix_second - rm_screenshot_after + 1)) ;
+    remote_end=$((remove_from + number_of_image_rm - 1)) ;
     echo "Start remove image from ${remove_from} to ${remove_end}" ;
     eval rm -f "/home/highside/screen/{$remove_from..$remote_end}.jpeg" ;
     ((screenshot_rm_timeout=0)) ;
